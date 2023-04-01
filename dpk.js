f9d5e0dd9837709fc263ecbd7edd443499548e92
Lucasd Daiki <lucasdaiki.dev@gmail.com>
2023-04-01 17:56:13 -0300

    feat(dpk): refactor deterministicPartitionKey function

--- dpk.js
+++ dpk.js
@@ -3,26 +3,31 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+  
+  const { partitionKey } = event;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!partitionKey) {
+    
+    const data = JSON.stringify(event);
+    
+    return generateHash(data);
+  }
+  
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    return generateHash(partitionKey);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof partitionKey !== "string") {
+    return JSON.stringify(partitionKey);
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
+  return partitionKey;
 };
+
+function generateHash(data) {
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
