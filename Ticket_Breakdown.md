8c464523a4643352e17def2fe57115a895fea6a8
Lucasd Daiki <lucasdaiki.dev@gmail.com>
2023-04-01 19:17:37 -0300

    chore(readme): update the ticketing breakdown

--- Ticket_Breakdown.md
+++ Ticket_Breakdown.md
@@ -16,3 +16,37 @@ Based on the information given, break this ticket down into 2-5 individual ticke
 You will be graded on the level of detail in each ticket, the clarity of the execution plan within and between tickets, and the intelligibility of your language. You don't need to be a native English speaker, but please proof-read your work.
 
 ## Your Breakdown Here
+# [Backend] Create a new unique field [custom_id] in the Agent table
+### Acceptance criteria: 
+ - [ ] The default value must the current ID;
+ - [ ] The fields must be unique; Max length = [TBD];
+### Time/effort estimates: 1 hour;
+### Implementation details: 
+ - the creation must be via migrations;
+
+# [Backend] Map the custom_id field at all Agent endpoints;
+### Acceptance criteria: 
+ - [ ] The custom_field must be visible in the GET /agents;
+ - [ ] must be changeable in POST /agents and PUT /agents;
+### Time/effort estimates: 4 hours;
+### Implementation details: 
+ - Update the getShiftsByFacility method; 
+ - Update the model;
+
+# [Frontend] Add a new field on agent section to fill the custom ID;
+### Acceptance criteria: 
+ - [ ] The fields must be a text field with max lenght = [TBD];
+### Time/effort estimates: 4 hours;
+### Implementation details: 
+ - Send the custom Id Field on POST and PUT /agents;
+ - Fill this field with the custom_id value;
+
+# [Backend] Replace the current ID field for the custom_id field into the function generateReport;
+### Acceptance criteria:
+ - [ ] The custom field must be appearing in the report at the same place of the current Id; 
+ - [ ] The current Id shall not be visible;
+### Time/effort estimates: 3 hours;
+
+# [Questions]
+ - Is it possible to replace all references to the current ID field for the custom_id?
+ - What is the custom_id max length?
--- dpk.test.js
+++ dpk.test.js
@@ -2,35 +2,35 @@ const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+    const partitionKey = deterministicPartitionKey();
+    expect(partitionKey).toBe("0");
   });
 
   it("Returns the partitionKey (string) when partition key is provided", () => {
     const event = { partitionKey: "testPartitionKey" };
 
-    const trivialKey = deterministicPartitionKey(event);
-    expect(trivialKey).toBe("testPartitionKey");
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toBe("testPartitionKey");
   });
 
   it("Returns the partitionKey (json) when partition key is provided", () => {
     const event = { partitionKey: { test: "PartitionKey" } };
 
-    const trivialKey = deterministicPartitionKey(event);
-    expect(trivialKey).toBe(JSON.stringify(event.partitionKey));
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toBe(JSON.stringify(event.partitionKey));
   });
 
   it("Returns a hash when partition key is not provided", () => {
     const event = { anotherField: { partition: "key" } };
 
-    const trivialKey = deterministicPartitionKey(event);
-    expect(trivialKey).toBe('de8bb975b898b524a6dc641acac54df5dbbd84320e8d87bca24098f17c754a20d3d7b6d131969cef58be21577a5833debbe8f673303649ee44b34b26f119909b');
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toMatchSnapshot();
   });
 
   it("Returns a hash when candidate is bigger than the MAX_PARTITION_KEY_LENGTH", () => {
     const event = { partitionKey: new Array(300).fill('a').join('') };
 
-    const trivialKey = deterministicPartitionKey(event);
-    expect(trivialKey).toBe('7350d99d1a20435c283070f3613302edb7027fced163086b048bd3ded530c5cb7a8ced83d1c6fda78f8832c61fb02698d14252c6b4ecf6989b81b04ca99a6302');
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toMatchSnapshot();
   });
 });
